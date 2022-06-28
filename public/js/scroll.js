0c3ce7940299c3b9d29260ec0e31f104defb4a1e
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-06-29 00:04:46 +0200

    Commit initial, création de la page cv

---
+++ public/js/scroll.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourPage'],
+    autoScrolling:true,
+    scrollingSpeed: 700,
+    easing: 'easeInOutCubic',
+    scrollHorizontally: true
+});
